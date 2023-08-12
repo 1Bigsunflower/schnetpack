6e9d86a7e43b5efcfa8e285602eb5f07fa0c2a29
cht <ti3552054@126.com>
2023-08-12 17:06:19 +0800

    中文注释

--- src/schnetpack/model/base.py
+++ src/schnetpack/model/base.py
@@ -9,6 +9,7 @@ from schnetpack.utils import as_dtype
 import torch
 import torch.nn as nn
 
+# __all__ 是一个特殊的变量，用于定义模块的公共接口。当使用 from module import * 导入模块时，只有在 __all__ 列表中列出的属性、函数、类等才会被导入。
 __all__ = ["AtomisticModel", "NeuralNetworkPotential"]
 
 
@@ -68,63 +69,63 @@ class AtomisticModel(nn.Module):
         Args:
             postprocessors: Post-processing transforms that may be
                 initialized using the `datamodule`, but are not
-                applied during training.
-            input_dtype: The dtype of real inputs as string.
-            do_postprocessing: If true, post-processing is activated.
+                applied during training.一个可选的后处理转换器列表
+            input_dtype: The dtype of real inputs as string.实数输入的数据类型
+            do_postprocessing: If true, post-processing is activated.表示是否启用后处理
         """
         super().__init__()
         self.input_dtype_str = input_dtype_str
-        self.do_postprocessing = do_postprocessing
-        self.postprocessors = nn.ModuleList(postprocessors)
-        self.required_derivatives: Optional[List[str]] = None
-        self.model_outputs: Optional[List[str]] = None
+        self.do_postprocessing = do_postprocessing  # 后处理标志
+        self.postprocessors = nn.ModuleList(postprocessors)  # 转化为模型列表
+        self.required_derivatives: Optional[List[str]] = None  # 指定在反向传播时需要计算梯度的输入键
+        self.model_outputs: Optional[List[str]] = None  # 指定模型的输出结果
 
-    def collect_derivatives(self) -> List[str]:
-        self.required_derivatives = None
-        required_derivatives = set()
-        for m in self.modules():
+    def collect_derivatives(self) -> List[str]:  # 收集需要在反向传播中计算梯度的输入键
+        self.required_derivatives = None  # 清除之前可能已经收集到的梯度输入键
+        required_derivatives = set()   # 存储要收集的梯度输入键
+        for m in self.modules():  # 遍历模型中模块
             if (
                 hasattr(m, "required_derivatives")
                 and m.required_derivatives is not None
-            ):
-                required_derivatives.update(m.required_derivatives)
-        required_derivatives: List[str] = list(required_derivatives)
+            ):  # 检查是否有 required_derivatives 属性，且不为None
+                required_derivatives.update(m.required_derivatives)  # 将模块的 required_derivatives 添加到 required_derivatives 集合中
+        required_derivatives: List[str] = list(required_derivatives)  # 转换为列表
         self.required_derivatives = required_derivatives
 
-    def collect_outputs(self) -> List[str]:
-        self.model_outputs = None
+    def collect_outputs(self) -> List[str]:  # 收集模型的输出结果
+        self.model_outputs = None  # 清除之前收集的模型输出结果
         model_outputs = set()
-        for m in self.modules():
-            if hasattr(m, "model_outputs") and m.model_outputs is not None:
+        for m in self.modules():  # 遍历模型模块（指的是各个层）
+            if hasattr(m, "model_outputs") and m.model_outputs is not None:  # 检查是否有 model_outputs 属性，并且该属性不为 None
                 model_outputs.update(m.model_outputs)
-        model_outputs: List[str] = list(model_outputs)
+        model_outputs: List[str] = list(model_outputs)  # 添加模型模块
         self.model_outputs = model_outputs
 
-    def initialize_derivatives(
+    def initialize_derivatives(  # 初始化输入张量的梯度，将输入张量中对应的张量启用梯度计算
         self, inputs: Dict[str, torch.Tensor]
     ) -> Dict[str, torch.Tensor]:
-        for p in self.required_derivatives:
-            if p in inputs.keys():
-                inputs[p].requires_grad_()
+        for p in self.required_derivatives:  # 遍历梯度的输出键列表
+            if p in inputs.keys():  # 判断这些输入键是否需要计算梯度  键:只包含名字。输入：既包含键又包含值（可以多个键多个值）
+                inputs[p].requires_grad_()  # 启用对该张量的梯度计算
         return inputs
 
-    def initialize_transforms(self, datamodule):
+    def initialize_transforms(self, datamodule):  # 为每个模块进行初始化操作
         for module in self.modules():
-            if isinstance(module, Transform):
+            if isinstance(module, Transform):  # 如果模块是一个转换类型的实例
                 module.datamodule(datamodule)
 
-    def postprocess(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
-        if self.do_postprocessing:
+    def postprocess(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:  # 后处理函数
+        if self.do_postprocessing:  # 后标志处理为真
             # apply postprocessing
-            for pp in self.postprocessors:
-                inputs = pp(inputs)
+            for pp in self.postprocessors:  # 遍历后处理器列表
+                inputs = pp(inputs)  # 对输入进行额外的处理
         return inputs
 
     def extract_outputs(
         self, inputs: Dict[str, torch.Tensor]
-    ) -> Dict[str, torch.Tensor]:
-        results = {k: inputs[k] for k in self.model_outputs}
-        return results
+    ) -> Dict[str, torch.Tensor]:  # 提取模型输出
+        results = {k: inputs[k] for k in self.model_outputs}  # 遍历模型输出中的键，将输入字典中对应键的值复制到结果字典中
+        return results  # 返回结果字典
 
 
 class NeuralNetworkPotential(AtomisticModel):
@@ -141,9 +142,9 @@ class NeuralNetworkPotential(AtomisticModel):
         representation: nn.Module,
         input_modules: List[nn.Module] = None,
         output_modules: List[nn.Module] = None,
-        postprocessors: Optional[List[Transform]] = None,
-        input_dtype_str: str = "float32",
-        do_postprocessing: bool = True,
+        postprocessors: Optional[List[Transform]] = None,  # 后处理转换列表
+        input_dtype_str: str = "float32",  # 输入数据类型
+        do_postprocessing: bool = True,  # 是否激活后处理
     ):
         """
         Args:
@@ -162,27 +163,28 @@ class NeuralNetworkPotential(AtomisticModel):
             postprocessors=postprocessors,
             do_postprocessing=do_postprocessing,
         )
-        self.representation = representation
-        self.input_modules = nn.ModuleList(input_modules)
-        self.output_modules = nn.ModuleList(output_modules)
+        self.representation = representation  # 从输入中构建表示的模块
+        self.input_modules = nn.ModuleList(input_modules)  # 在表示之前应用的模块，例如修改输入或添加额外的张量以获取响应属性
+        self.output_modules = nn.ModuleList(output_modules)  # 预测输出属性的模块
 
-        self.collect_derivatives()
-        self.collect_outputs()
+        self.collect_derivatives()  # 收集需要在反向传播中计算梯度的输入键
+        self.collect_outputs()  # 收集输出
 
     def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
+        # 接受一个包含输入数据的字典作为参数，并返回一个包含输出结果的字典。
         # initialize derivatives for response properties
-        inputs = self.initialize_derivatives(inputs)
+        inputs = self.initialize_derivatives(inputs)  # 初始化输入张量的梯度，将输入张量中对应的张量启用梯度计算
 
-        for m in self.input_modules:
+        for m in self.input_modules:  # 依次处理输入模块列表中的每个模块
             inputs = m(inputs)
 
-        inputs = self.representation(inputs)
+        inputs = self.representation(inputs)  # 从输入中构建表示的模块
 
-        for m in self.output_modules:
+        for m in self.output_modules:  # 预测输出属性
             inputs = m(inputs)
 
         # apply postprocessing (if enabled)
-        inputs = self.postprocess(inputs)
-        results = self.extract_outputs(inputs)
+        inputs = self.postprocess(inputs)  # 如果启用后处理功能，对inputs进行后处理
+        results = self.extract_outputs(inputs)  # 从inputs中提取输出结果
 
         return results
--- src/schnetpack/train/callbacks.py
+++ src/schnetpack/train/callbacks.py
@@ -14,7 +14,9 @@ from schnetpack.task import AtomisticTask
 from schnetpack import properties
 from collections import defaultdict
 
-
+# "ModelCheckpoint"：该回调类用于保存训练过程中的最佳模型
+# "PredictionWriter"：用于在训练过程中保存模型的预测结果
+# "ExponentialMovingAverage"：用于实现指数移动平均，是一种平滑数据的方法
 __all__ = ["ModelCheckpoint", "PredictionWriter", "ExponentialMovingAverage"]
 
 
@@ -25,9 +27,9 @@ class PredictionWriter(BasePredictionWriter):
 
     def __init__(
             self,
-            output_dir: str,
-            write_interval: str,
-            write_idx: bool = False,
+            output_dir: str,  # 输出目录
+            write_interval: str,  # 写入间隔（batch、epoch等
+            write_idx: bool = False,  # 是否写入分子id
     ):
         """
         Args:
@@ -39,21 +41,21 @@ class PredictionWriter(BasePredictionWriter):
         super().__init__(write_interval)
         self.output_dir = output_dir
         self.write_idx = write_idx
-        os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(output_dir, exist_ok=True)  # 如果不存在，则创建名为output_dir的目录
 
     def write_on_batch_end(
         self,
-        trainer,
-        pl_module: AtomisticTask,
-        prediction: Any,
-        batch_indices: List[int],
-        batch: Any,
-        batch_idx: int,
-        dataloader_idx: int,
+        trainer,  # 训练器
+        pl_module: AtomisticTask,  # atomistictask模型
+        prediction: Any,  # 预测结果
+        batch_indices: List[int],  # 批次索引列表
+        batch: Any,  # 批次数据
+        batch_idx: int,  # 批次索引
+        dataloader_idx: int,  # 数据加载索引
     ):
-        bdir = os.path.join(self.output_dir, str(dataloader_idx))
-        os.makedirs(bdir, exist_ok=True)
-        torch.save(prediction, os.path.join(bdir, f"{batch_idx}.pt"))
+        bdir = os.path.join(self.output_dir, str(dataloader_idx))  # 根据dataloader_idx创建目录bdir
+        os.makedirs(bdir, exist_ok=True)  # 确保目录存在
+        torch.save(prediction, os.path.join(bdir, f"{batch_idx}.pt"))  # 将prediction保存为一个.pt文件，保存在目录bdir下
 
     def write_on_epoch_end(
         self,
@@ -63,18 +65,18 @@ class PredictionWriter(BasePredictionWriter):
         batch_indices: List[Any],
     ):
         # collect batches of predictions and restructure
-        concatenated_predictions = defaultdict(list)
-        for batch_prediction in predictions[0]:
+        concatenated_predictions = defaultdict(list)  # 用于存储预测结果，defaultdict(list)可以方便地将数据按照属性名进行分组
+        for batch_prediction in predictions[0]:  # 遍历预测列表中每个batch_prediction
             for property_name, data in batch_prediction.items():
                 if not self.write_idx and property_name == properties.idx_m:
                     continue
-                concatenated_predictions[property_name].append(data)
-        concatenated_predictions = {
+                concatenated_predictions[property_name].append(data)  # 将数据添加到concatenated_predictions字典对应属性的列表中
+        concatenated_predictions = {  # 对 concatenated_predictions 字典中的数据进行拼接
             property_name: torch.concat(data)
             for property_name, data in concatenated_predictions.items()
         }
 
-        # save concatenated predictions
+        # save concatenated predictions，保存到output_dir路径下的predictions.pt文件中
         torch.save(
             concatenated_predictions,
             os.path.join(self.output_dir, "predictions.pt"),
@@ -89,28 +91,29 @@ class ModelCheckpoint(BaseModelCheckpoint):
 
     def __init__(self, model_path: str, do_postprocessing=True, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.model_path = model_path
-        self.do_postprocessing = do_postprocessing
+        self.model_path = model_path  # 保存模型的路径
+        self.do_postprocessing = do_postprocessing  # 是否进行后处理
 
-    def on_validation_end(self, trainer, pl_module: AtomisticTask) -> None:
+    def on_validation_end(self, trainer, pl_module: AtomisticTask) -> None:  # 在验证结束后调用的方法，用于设置训练器和任务
         self.trainer = trainer
         self.task = pl_module
-        super().on_validation_end(trainer, pl_module)
+        super().on_validation_end(trainer, pl_module)  # super().on_validation_end(trainer, pl_module)
 
-    def _update_best_and_save(
-        self, current: torch.Tensor, trainer, monitor_candidates: Dict[str, Any]
+    def _update_best_and_save(  # 更新最佳模型并保存的方法
+        self, current: torch.Tensor, trainer, monitor_candidates: Dict[str, Any]  # current:当前模型评分，monitor：监控指标的候选值
     ):
         # save model checkpoint
         super()._update_best_and_save(current, trainer, monitor_candidates)
 
         # save best inference model
+        # 如果current的类型是torch.Tensor并且是NaN值，将其替换为正无穷大（如果模式为"min"）或负无穷大（如果模式为"max"）
         if isinstance(current, torch.Tensor) and torch.isnan(current):
             current = torch.tensor(float("inf" if self.mode == "min" else "-inf"))
 
-        if current == self.best_model_score:
-            if self.trainer.strategy.local_rank == 0:
+        if current == self.best_model_score:  # 判断是否模型和最佳模型评分是否相等
+            if self.trainer.strategy.local_rank == 0:  # 是否是本地模型
                 # remove references to trainer and data loaders to avoid pickle error in ddp
-                self.task.save_model(self.model_path, do_postprocessing=True)
+                self.task.save_model(self.model_path, do_postprocessing=True)  # 保存模型到指定路径，并进行后处理操作
 
 
 class ExponentialMovingAverage(Callback):
