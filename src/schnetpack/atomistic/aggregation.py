da0bd22e8a2ed02bd233fcc7b7cf62d1851856b3
cht <ti3552054@126.com>
2023-08-18 09:12:46 +0800

    comments #_3：Aggregate predictions

--- src/schnetpack/atomistic/aggregation.py
+++ src/schnetpack/atomistic/aggregation.py
@@ -16,6 +16,8 @@ class Aggregation(nn.Module):
     """
 
     def __init__(self, keys: List[str], output_key: str = "y"):
+        # keys 表示要进行相加的属性名称
+        # output_key 表示输出中新属性的名称
         super(Aggregation, self).__init__()
 
         self.keys: List[str] = list(keys)
@@ -25,4 +27,5 @@ class Aggregation(nn.Module):
     def forward(self, inputs: Dict[str, torch.Tensor]) -> Dict[str, torch.Tensor]:
         energy = torch.stack([inputs[key] for key in self.keys]).sum(0)
         inputs[self.output_key] = energy
+        # 计算聚合后的能量
         return inputs
